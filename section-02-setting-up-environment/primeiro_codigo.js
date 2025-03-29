c38a73562bf988c7172bd2f9a05279b5c27a71bd
Rogonrodrigues3942 <rogonrodrigues@gmail.com>
2025-03-29 20:04:07 -0300

    Transpile primeiro_codigo.ts - 2nd time

--- section-02-setting-up-environment/primeiro_codigo.js
+++ section-02-setting-up-environment/primeiro_codigo.js
@@ -1,1 +1,4 @@
 console.log('Hello World! é o primeiro código do curso!!!');
+console.log('==============================================');
+var nome = 'Rogério';
+console.log('Olá ' + nome + '. Seja benvindo!');
--- section-02-setting-up-environment/primeiro_codigo.ts
+++ section-02-setting-up-environment/primeiro_codigo.ts
@@ -1,1 +1,7 @@
 console.log('Hello World! é o primeiro código do curso!!!')
+
+console.log('==============================================');
+
+const nome = 'Rogério';
+
+console.log('Olá ' + nome + '. Seja benvindo!');
