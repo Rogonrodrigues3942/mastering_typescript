8118e54722c46243bccac9b74fded12f07a7c2c6
Rogonrodrigues3942 <rogonrodrigues@gmail.com>
2025-03-30 12:37:57 -0300

    modified:   primeiro_codigo.js

--- section-02-setting-up-environment/primeiro_codigo.js
+++ section-02-setting-up-environment/primeiro_codigo.js
@@ -2,3 +2,12 @@ console.log('Hello World! é o primeiro código do curso!!!');
 console.log('==============================================');
 var nome = 'Rogério';
 console.log('Olá ' + nome + '. Seja benvindo!');
+console.log('==============================================');
+var Produto = /** @class */ (function () {
+    // TODO: método construtor - usado para inicialização.
+    function Produto(produtoNome, produtoValor) {
+        this.nome = produtoNome;
+        this.valor = produtoValor;
+    }
+    return Produto;
+}());
