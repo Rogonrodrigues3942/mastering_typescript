b6b0f728c49ed3a7a84bdf4264cd680eff980f55
Rogonrodrigues3942 <rogonrodrigues@gmail.com>
2025-03-30 12:30:01 -0300

    modified:   section-02-setting-up-environment/primeiro_codigo.ts

--- section-02-setting-up-environment/primeiro_codigo.ts
+++ section-02-setting-up-environment/primeiro_codigo.ts
@@ -5,3 +5,19 @@ console.log('==============================================');
 const nome = 'Rogério';
 
 console.log('Olá ' + nome + '. Seja benvindo!');
+
+console.log('==============================================');
+
+class Produto{
+
+    nome: string;
+    valor:number;
+
+    // TODO: método construtor - usado para inicialização.
+    constructor( produtoNome, produtoValor){
+
+        this.nome = produtoNome;
+        this.valor = produtoValor;
+
+    }
+}
