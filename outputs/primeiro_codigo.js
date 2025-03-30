daa770f906abf61a237619bf5828126d5115a317
Rogonrodrigues3942 <rogonrodrigues@gmail.com>
2025-03-30 13:46:33 -0300

    Upgate 2-->   project

---
+++ outputs/primeiro_codigo.js
@@ -0,0 +1,15 @@
+"use strict";
+console.log('Hello World! é o primeiro código do curso!!!');
+console.log('==============================================');
+const nome = 'Rogério';
+console.log('Olá ' + nome + '. Seja benvindo!');
+console.log('==============================================');
+class Produto {
+    // TODO: método construtor - usado para inicialização.
+    constructor(produtoNome, produtoValor) {
+        this.nome = produtoNome;
+        this.valor = produtoValor;
+    }
+}
+console.log('==============================================');
+const playstation5 = new Produto('Playstation 5', 5000);
