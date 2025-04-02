43966ae063fe20fc32b7943cb60f362cd343db9a
Rogonrodrigues3942 <rogonrodrigues@gmail.com>
2025-04-01 22:08:39 -0300

    Add array e update files.

---
+++ outputs/array.js
@@ -0,0 +1,11 @@
+"use strict";
+const filmesCategorias = ['Comédia', 'Drama', 'Romance', 'Aventura'];
+console.log(filmesCategorias);
+filmesCategorias.push('Terror');
+console.log(filmesCategorias);
+const filmesAnos = [];
+for (let ano = 1990; ano <= new Date().getFullYear(); ano++) {
+    filmesAnos.push(ano);
+}
+console.log(filmesAnos);
+//# sourceMappingURL=array.js.map
---
+++ outputs/boolean.js
@@ -0,0 +1,12 @@
+"use strict";
+// TODO: BOOLEAN
+let admin = true;
+admin = false;
+admin = Boolean('1');
+console.log(admin);
+// TODO: Verificando senha
+let senha = '1234';
+let senhaSegura;
+senhaSegura = senha.length > 8;
+console.log(senhaSegura);
+//# sourceMappingURL=boolean.js.map
