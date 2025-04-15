daee030a3d7a15d09c5fb64e46c1458813c5f0be
Rogonrodrigues3942 <rogonrodrigues@gmail.com>
2025-04-15 08:57:20 -0300

    update  outputs/unknown.js, outputs/unknown.js.map e scripts/unknown.ts

---
+++ outputs/unknown.js
@@ -0,0 +1,15 @@
+"use strict";
+let resultado;
+resultado = {
+    success: true
+};
+resultado = 'Deu tudo certo. Ok!';
+resultado = 200;
+console.log('----------------------------------------------');
+const informacao = 150;
+const informacaoDesconhecida = 250;
+const primeiroNumero = informacao;
+const segundoNumero = informacaoDesconhecida;
+console.log(primeiroNumero);
+console.log(segundoNumero);
+//# sourceMappingURL=unknown.js.map
---
+++ scripts/unknown.ts
@@ -0,0 +1,19 @@
+let resultado;
+
+resultado = {
+    success: true
+}
+
+resultado = 'Deu tudo certo. Ok!';
+resultado = 200;
+
+console.log('----------------------------------------------');
+
+const informacao: any = 150;
+const informacaoDesconhecida: unknown = 250;
+
+const primeiroNumero: number = informacao;
+const segundoNumero: number = informacaoDesconhecida;
+
+console.log(primeiroNumero);
+console.log(segundoNumero);
