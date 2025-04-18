3170a2c730088abe41d488ce5c2fd049caa1ee07
Rogonrodrigues3942 <rogonrodrigues@gmail.com>
2025-04-18 12:18:31 -0300

    Inserindo arquivos JS.

---
+++ outputs/type_assertion.js
@@ -0,0 +1,6 @@
+"use strict";
+const site = 'https://hcode.com.br';
+let sitesFavoritos = [];
+sitesFavoritos.push(site); //TODO: Realizado casting através da palavara reservada 'as'.
+sitesFavoritos.push(site); //TODO: Realizado casting através do comando <tipo_dado> antes do parâmetro.
+//# sourceMappingURL=type_assertion.js.map
