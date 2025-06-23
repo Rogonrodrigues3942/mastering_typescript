43276b6ab79d3d092dc012450923b800985ade76
Rogonrodrigues3942 <rogonrodrigues@gmail.com>
2025-06-23 20:11:51 -0300

    Update files classe_readonly

---
+++ outputs/classe_readonly.js
@@ -0,0 +1,14 @@
+"use strict";
+class Usuario {
+    constructor() {
+        this.id = '12345sda6789';
+        this.nome = 'Rogério';
+        this.senha = '678901';
+        this.dataCadastro = new Date('2021-01-31');
+    }
+}
+const usuario = new Usuario;
+console.log(usuario.id);
+// usuario.id = '8907rsa';
+// console.log(usuario.id);
+//# sourceMappingURL=classe_readonly.js.map
---
+++ scripts/classe_readonly.ts
@@ -0,0 +1,13 @@
+class Usuario{
+    readonly id:string = '12345sda6789';
+    nome: string = 'Rogério';
+    private senha: string = '678901';
+    readonly dataCadastro:Date = new Date('2021-01-31');
+}
+
+const usuario = new Usuario;
+
+console.log(usuario.id);
+
+// usuario.id = '8907rsa';
+// console.log(usuario.id);
