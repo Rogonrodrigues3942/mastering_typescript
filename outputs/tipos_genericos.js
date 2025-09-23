2ae34884e073d28f40518c4bd22d020203755a41
Rogonrodrigues3942 <rogonrodrigues@gmail.com>
2025-09-23 08:04:53 -0300

    Updating files tipos_genericos.ts, tipos_genericos.js.map e tipos_genericos.js

---
+++ outputs/tipos_genericos.js
@@ -0,0 +1,6 @@
+"use strict";
+function obterPrimeiraPosicao(meuArray) {
+    return meuArray[0];
+}
+console.log(obterPrimeiraPosicao(['Rogério', 'Rafael', 'Samuel']));
+//# sourceMappingURL=tipos_genericos.js.map
---
+++ scripts/tipos_genericos.ts
@@ -0,0 +1,7 @@
+function obterPrimeiraPosicao<Teste>(meuArray: Array<Teste>): Teste{
+
+
+    return meuArray[0];
+}
+
+console.log(obterPrimeiraPosicao(['Rogério', 'Rafael', 'Samuel']));
