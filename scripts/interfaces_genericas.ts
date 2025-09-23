afc1ef23c896b152ac0e577826382d1aafed86ce
Rogonrodrigues3942 <rogonrodrigues@gmail.com>
2025-09-23 08:33:41 -0300

    Updating version 2: files tipos_genericos.ts, tipos_genericos.js.map e tipos_genericos.js

---
+++ scripts/interfaces_genericas.ts
@@ -0,0 +1,30 @@
+
+interface IProcessamento<TipoInterface>{
+
+    valor: TipoInterface;
+
+    realizarProcessamento(arg: TipoInterface): TipoInterface;
+}
+
+const texto: IProcessamento<string> = {
+
+    valor: 'hcode',
+    realizarProcessamento(argumento: string): string{
+        return argumento.toUpperCase();
+    }
+}
+
+console.log(texto.valor);
+console.log(texto.realizarProcessamento('hcode treinamentos'));
+
+const numero: IProcessamento<number> ={
+
+    valor:10,
+    realizarProcessamento(v: number): number{
+        return v * v;
+    }
+}
+
+console.log(numero.valor);
+console.log(numero.realizarProcessamento(50));
+console.log(numero.realizarProcessamento(numero.valor));
